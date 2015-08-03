3d0d9bd426b0046fea69f44535590355bfb20cf9
Cyril Montailler <cmonta@macbook-pro-de-cyril.home>
2015-08-03 18:22:52 +0200

    Registration start

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,2 +1,4 @@
 class Race < ActiveRecord::Base
+	has_many :registrations
+	has_many :users, through: :registrations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :registrations
+	has_many :races, through: :registrations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
