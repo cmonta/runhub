7fb88506b26c2b7a7c869c5290b70584681cb2d9
Cyril Montailler <cmonta@free.fr>
2015-08-04 22:59:43 +0200

    Added extended fields for users edit and creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :races
-  devise_for :users
+  resources :users, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
